bddd8800bafe96dd884cbc1f65668fff3cc1583e
Hari Gopal <mail@harigopal.in>
2023-05-03 20:43:09 +0530

    Try to require SSL for DB connection in prod

--- config/config.js
+++ config/config.js
@@ -17,5 +17,11 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+   },
   },
 };
