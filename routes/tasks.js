58ad288e474c608b565b996154dca303f50da83f
Melson Zacharias <jmelson7@gmail.com>
2023-05-25 16:36:26 +0530

    Fix enum state

--- routes/tasks.js
+++ routes/tasks.js
@@ -23,7 +23,7 @@ router.get("/", async function (req, res, next) {
     },
     {
       coloumns: {
-        new: {
+        pending: {
           id: "pending",
           title: "Pending",
           taskIDs: [],
